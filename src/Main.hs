0f3e1d38e49754c058f640ab90584a6828ba3944
Attox <attox@protonmail.com>
2018-04-09 05:11:56 +0200

    switched to Gui Builder and gtk3

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,12 @@
-module Main where
+module Main
+where
 
 import           Data.List.Split
-import qualified Data.Text                 as DT
+import qualified Data.Text                     as DT
 import           Data.Maybe
 import           System.Process
 import           Graphics.UI.Gtk
-import           Graphics.UI.Gtk.ModelView as Model
+import           Graphics.UI.Gtk.Builder
 
 
 data Device = Device { name     :: String
@@ -26,12 +27,17 @@ parseDevices deviceList =
     $ splitOn "\n" deviceList
 
 createDevRecord :: [String] -> Device
-createDevRecord deviceEntry = Device {name = a, deviceId = b, status = status, values= v}
+createDevRecord deviceEntry = Device
+  { name     = a
+  , deviceId = b
+  , status   = status
+  , values   = v
+  }
  where
   (name : id : status : rest) = deviceEntry
   a                           = formatName name
   b                           = formatId id
-  v = [""]
+  v                           = [""]
 
 getDevices :: IO [Device]
 getDevices = do
@@ -39,23 +45,21 @@ getDevices = do
   return $ map createDevRecord . parseDevices $ init n
 
 getProperties :: Device -> IO Device
-getProperties Device { name=n, status=s, deviceId=i} = do
+getProperties Device { name = n, status = s, deviceId = i } = do
   props <- readProcess "xinput" ["list-props", i] []
   let properties = map (DT.unpack . DT.strip . DT.pack) $ lines props
-  return $ Device {name=n, status=s, deviceId=i, values=properties }
+  return $ Device {name = n, status = s, deviceId = i, values = properties}
 
-onSelection :: ListStore Device -> Model.TreeSelection -> ComboBox -> IO ()
-onSelection list tree textview = do
+onSelection :: ListStore Device -> TreeSelection -> ListStore DT.Text -> IO ()
+onSelection list tree propertyList = do
   -- clear combox
-  n <- comboBoxGetModelText textview
-  listStoreClear n
-  comboBoxSetModelText textview
+  listStoreClear propertyList
   -- populate with properties of selected device
-  sel <- Model.treeSelectionGetSelectedRows tree
+  sel <- treeSelectionGetSelectedRows tree
   let s = head (head sel)
-  v     <- Model.listStoreGetValue list s
+  v     <- listStoreGetValue list s
   props <- getProperties v
-  mapM_ (comboBoxAppendText textview . DT.pack) $ values props
+  mapM_ (listStoreAppend propertyList. DT.pack) $ values props
 
 populateLabels :: ComboBoxText -> String -> (String, String)
 populateLabels properties device =
@@ -68,11 +72,12 @@ setLabels d p (deviceL, propL) = do
   entrySetText d deviceL
   entrySetText p propL
 
-currentPropertyIds :: ListStore Device -> TreeSelection -> ComboBox -> Entry -> Entry -> IO ()
+currentPropertyIds
+  :: ListStore Device -> TreeSelection -> ComboBox -> Entry -> Entry -> IO ()
 currentPropertyIds list tree textbox deviceL propL = do
-  sel         <- Model.treeSelectionGetSelectedRows tree
+  sel         <- treeSelectionGetSelectedRows tree
   selProperty <- comboBoxGetActiveText textbox
-  v           <- Model.listStoreGetValue list (head (head sel))
+  v           <- listStoreGetValue list (head (head sel))
   props       <- getProperties v
   case selProperty of
     Nothing -> return ()
@@ -81,72 +86,53 @@ currentPropertyIds list tree textbox deviceL propL = do
 
 run :: Entry -> Entry -> Entry -> IO ()
 run devId propId newVal = do
-  d       <- entryGetText devId :: IO String
-  p       <- entryGetText propId :: IO String
-  v       <- entryGetText newVal :: IO String
+  d       <- entryGetText devId 
+  p       <- entryGetText propId 
+  v       <- entryGetText newVal 
   procOut <- readProcess "xinput" ["set-prop", d, p, v] []
   print procOut
 
 main :: IO ()
 main = do
-  -- get our device list
-  n <- getDevices
-  -- setup basic UI elements
+  -- get our device list and init builder
   initGUI
-  window         <- windowNew
-  vbox           <- vBoxNew False 10
-  hbox           <- hBoxNew True 10
-  devLabel       <- labelNew (Just "Device ID:")
-  propLabel      <- labelNew (Just "Property ID:")
-  valLabel       <- labelNew (Just "new Value:")
-  changeDevice   <- entryNew
-  changeProperty <- entryNew
-  newValue       <- entryNew
-  commitChange   <- buttonNewWithLabel "Change!"
+  n       <- getDevices
+  builder <- builderNew
+  builderAddFromFile builder "./gui.glade"
+  -- setup basic UI elements
   list           <- listStoreNew n --store this
+  window         <- builderGetObject builder castToWindow "window1"
+  devLabel       <- builderGetObject builder castToLabel "label1"
+  propLabel      <- builderGetObject builder castToLabel "label2"
+  valLabel       <- builderGetObject builder castToLabel "label3"
+  changeDevice   <- builderGetObject builder castToEntry "entry1"
+  changeProperty <- builderGetObject builder castToEntry "entry2"
+  newValue       <- builderGetObject builder castToEntry "entry3"
+  commitChange   <- builderGetObject builder castToButton "button1"
+  box <- builderGetObject builder castToBox "box3"
   combobox       <- comboBoxNewText
-  treeview       <- Model.treeViewNewWithModel list
-  scrwinTree     <- scrolledWindowNew Nothing Nothing
+  text <- comboBoxGetModelText combobox
+  treeview       <- builderGetObject builder castToTreeView "treeview1"
   -- treview that stores all our devices
-  col            <- Model.treeViewColumnNew -- column for our content
-  renderer       <- Model.cellRendererTextNew
-  Model.cellLayoutPackStart     col renderer True
-  Model.cellLayoutSetAttributes col renderer list
-    $ \d -> [Model.cellText := name d ++ "\t ID:" ++ deviceId d]
-  Model.treeViewAppendColumn treeview col
-  tree <- Model.treeViewGetSelection treeview
-  Model.treeSelectionSetMode    tree       SelectionSingle
-  -- embed treeview in scrolled windows
-  scrolledWindowAddWithViewport scrwinTree treeview
-  scrolledWindowSetPolicy scrwinTree PolicyAutomatic PolicyAutomatic
+  treeViewSetModel treeview list
+  col      <- treeViewColumnNew -- column for our content
+  renderer <- cellRendererTextNew
+  cellLayoutPackStart col renderer True
+  cellLayoutSetAttributes col renderer list
+    $ \d -> [cellText := name d ++ "\t ID:" ++ deviceId d]
+  treeViewAppendColumn treeview col
+  tree <- treeViewGetSelection treeview
+  treeSelectionSetMode tree SelectionSingle
   --  events
-  Model.onSelectionChanged tree (onSelection list tree combobox)
+  on treeview cursorChanged (onSelection list tree text)
   commitChange `on` buttonActivated $ run changeDevice changeProperty newValue
   combobox `on` changed $ currentPropertyIds list
                                              tree
                                              combobox
                                              changeDevice
                                              changeProperty
-  -- create containers
-  set
-    window
-    [ windowDefaultWidth := 800
-    , windowDefaultHeight := 600
-    , windowResizable := True
-    , containerBorderWidth := 10
-    , containerChild := vbox
-    ]
-  boxPackStart vbox combobox       PackNatural 0
-  boxPackStart vbox scrwinTree     PackGrow    0
-  boxPackStart hbox devLabel       PackGrow    0
-  boxPackStart hbox changeDevice   PackGrow    0
-  boxPackStart hbox propLabel      PackGrow    0
-  boxPackStart hbox changeProperty PackGrow    0
-  boxPackStart hbox valLabel       PackGrow    0
-  boxPackStart hbox newValue       PackGrow    0
-  boxPackStart hbox commitChange   PackGrow    0
-  boxPackStart vbox hbox           PackNatural 0
+                                             
+  boxPackStart box combobox PackNatural 0
   -- run GUI
-  onDestroy window mainQuit
   widgetShowAll window
   mainGUI
